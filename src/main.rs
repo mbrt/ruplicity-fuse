4080f4579c82ccb8a6392fad33ecfa07d9bda2e8
Michele Bertasi <michele.bertasi@gmail.com>
2015-12-19 17:30:36 +0000

    Use chan-signal to handle term signals and unmount

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 extern crate fuse;
+extern crate chan_signal;
+
 mod fs;
 
 use std::env;
+use chan_signal::Signal;
+
 use fs::RuplicityFs;
 
 fn main() {
@@ -12,5 +16,13 @@ fn main() {
             return;
         }
     };
-    fuse::mount(RuplicityFs, &mountpoint, &[]);
+
+    let signal = chan_signal::notify(&[Signal::INT, Signal::TERM]);
+    let _mount = unsafe {
+        fuse::spawn_mount(RuplicityFs, &mountpoint, &[])
+    };
+
+    // Blocks until this process is sent an INT or TERM signal.
+    // Since the channel is never closed, we can unwrap the received value.
+    signal.recv().unwrap();
 }
