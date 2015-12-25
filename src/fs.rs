36454c9a0684d09673fddbe5558550d9d3d554ad
Michele Bertasi <michele.bertasi@gmail.com>
2015-12-25 22:29:47 +0000

    First draft of snapshot attr and lookup

--- src/fs.rs
+++ src/fs.rs
@@ -1,18 +1,35 @@
-use fuse::{FileAttr, FileType, Filesystem, ReplyAttr, ReplyDirectory, Request};
+use fuse::{FileAttr, FileType, Filesystem, ReplyAttr, ReplyEntry, ReplyDirectory, Request};
 use libc::{ENOENT, ENOSYS};
 use time;
-use ruplicity::Backup;
-use ruplicity::backend::local::LocalBackend;
+use ruplicity::{Backend, Backup};
 
+use std::collections::HashMap;
 use std::path::Path;
 
-pub struct RuplicityFs {
-    backup: Backup<LocalBackend>,
+
+pub struct RuplicityFs<B> {
+    backup: Backup<B>,
+    snapshots_ino: (u64, u64),
+    snapshots_paths: HashMap<String, usize>,
 }
 
-impl RuplicityFs {
-    pub fn new(backup: Backup<LocalBackend>) -> Self {
-        RuplicityFs { backup: backup }
+impl<B: Backend> RuplicityFs<B> {
+    pub fn new(backup: Backup<B>) -> Self {
+        let mut spaths = HashMap::new();
+        let mut count = 0;
+        for snapshot in backup.snapshots().unwrap() {
+            let time = time::at(snapshot.time());
+            let path = time::strftime("%Y-%m-%d_%H:%M:%S", &time).unwrap();
+            spaths.insert(path, count);
+            count += 1;
+        }
+        let num_snapshots = spaths.len();
+
+        RuplicityFs {
+            backup: backup,
+            snapshots_ino: (2, num_snapshots as u64 + 2),
+            snapshots_paths: spaths,
+        }
     }
 
     fn getattr_root(&mut self, reply: ReplyAttr) {
@@ -36,6 +53,35 @@ impl RuplicityFs {
         reply.attr(&ts, &attr);
     }
 
+    fn getattr_snapshot(&mut self, ino: u64, reply: ReplyAttr) {
+        match try_or_log!(self.backup.snapshots()).nth(ino as usize - 2) {
+            Some(snapshot) => {
+                let ts = snapshot.time();
+                let attr = FileAttr {
+                    ino: ino,
+                    size: 0,
+                    blocks: 0,
+                    atime: ts,
+                    mtime: ts,
+                    ctime: ts,
+                    crtime: ts,
+                    kind: FileType::Directory,
+                    perm: 0o755,
+                    nlink: 0,
+                    uid: 0,
+                    gid: 0,
+                    rdev: 0,
+                    flags: 0,
+                };
+                reply.attr(&ts, &attr);
+            }
+            None => {
+                error!("Cannot find snapshot for ino {}", ino);
+                reply.error(ENOSYS);
+            }
+        }
+    }
+
     fn readdir_root(&mut self, mut offset: u64, mut reply: ReplyDirectory) {
         if offset == 0 {
             reply.add(1, 0, FileType::Directory, &Path::new("."));
@@ -45,7 +91,7 @@ impl RuplicityFs {
             for snapshot in snapshots {
                 let time = time::at(snapshot.time());
                 let path = try_or_log!(time::strftime("%Y-%m-%d_%H:%M:%S", &time));
-                if reply.add(1, offset, FileType::Directory, &Path::new(&path)) {
+                if reply.add(offset, offset, FileType::Directory, &Path::new(&path)) {
                     break;
                 }
                 offset += 1;
@@ -53,12 +99,54 @@ impl RuplicityFs {
         }
         reply.ok();
     }
+
+    fn lookup_snapshot(&mut self, name: &Path, reply: ReplyEntry) {
+        let sid = match self.snapshots_paths.get(name.to_str().unwrap()) {
+            Some(id) => *id,
+            None => {
+                reply.error(ENOENT);
+                return;
+            }
+        };
+        match try_or_log!(self.backup.snapshots()).nth(sid) {
+            Some(snapshot) => {
+                let ts = snapshot.time();
+                let attr = FileAttr {
+                    ino: sid as u64 + 2,
+                    size: 0,
+                    blocks: 0,
+                    atime: ts,
+                    mtime: ts,
+                    ctime: ts,
+                    crtime: ts,
+                    kind: FileType::Directory,
+                    perm: 0o755,
+                    nlink: 0,
+                    uid: 0,
+                    gid: 0,
+                    rdev: 0,
+                    flags: 0,
+                };
+                reply.entry(&ts, &attr, 0);
+            }
+            None => {
+                reply.error(ENOENT);
+                return;
+            }
+        };
+    }
+
+    fn is_snapshot(&self, ino: u64) -> bool {
+        self.snapshots_ino.0 <= ino && ino < self.snapshots_ino.1
+    }
 }
 
-impl Filesystem for RuplicityFs {
+impl<B: Backend> Filesystem for RuplicityFs<B> {
     fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
         if ino == 1 {
             self.getattr_root(reply);
+        } else if self.is_snapshot(ino) {
+            self.getattr_snapshot(ino, reply);
         } else {
             reply.error(ENOSYS);
         }
@@ -67,6 +155,14 @@ impl Filesystem for RuplicityFs {
     fn readdir(&mut self, _req: &Request, ino: u64, _fh: u64, offset: u64, reply: ReplyDirectory) {
         if ino == 1 {
             self.readdir_root(offset, reply);
+        } else {
+            reply.error(ENOENT);
+        }
+    }
+
+    fn lookup(&mut self, _req: &Request, parent: u64, name: &Path, reply: ReplyEntry) {
+        if parent == 1 {
+            self.lookup_snapshot(name, reply);
         } else {
             reply.error(ENOENT);
         }
